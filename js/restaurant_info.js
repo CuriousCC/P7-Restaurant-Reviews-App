261cc262a143e62bbaf8734a2637bb3338e932c6
CuriousCC <spplearn@gmail.com>
2019-03-13 03:40:28 +0000

    add aria attributes for restaurant details page

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -90,9 +90,10 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   image.alt = DBHelper.altTxtForRestaurantImage(restaurant);
-  
+
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
+  cuisine.setAttribute('aria-label', 'Cuisine type');
 
   // fill operating hours
   if (restaurant.operating_hours) {
@@ -167,6 +168,7 @@ createReviewHTML = (review) => {
   const rating = document.createElement('p');
   rating.className = 'review-rating';
   rating.innerHTML = `Rating: ${review.rating}`;
+  rating.setAttribute('aria-label', `Commenter's rating: ${review.rating}`);
   li.appendChild(rating);
 
   const comments = document.createElement('p');
