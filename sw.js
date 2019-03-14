3b437af0dab2dfade04894ebc5e986fd70cc99f1
CuriousCC <spplearn@gmail.com>
2019-03-14 21:19:49 +0000

    format code and expand code at (line 50) so that if a request doesn't match anything in the cache, get it from the network, send
    as explained in
    https://developers.google.com/web/ilt/pwa/caching-files-with-service-worker

--- sw.js
+++ sw.js
@@ -44,11 +44,17 @@ self.addEventListener('activate', (event) => {
     );
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                return response || fetch(event.request);
-            })
-    );
-});
+self.addEventListener('fetch', (event) => { 
+        event.respondWith(
+            caches.open(cacheVersion)
+            .then((cache) => { 
+                return cache.match(event.request)
+                .then((response) => { 
+                    return response || fetch(event.request)
+                    .then((response) => { 
+                        cache.put(event.request, response.clone()); 
+                        return response; 
+                    }); 
+                }); 
+            })); 
+        });
