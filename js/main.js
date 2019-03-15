822fa903ec88d802b81e6ea60d01ac17df6a2f3c
CuriousCC <spplearn@gmail.com>
2019-03-15 01:11:54 +0000

    change path to navigator (sw reg)

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
  */
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
+  navigator.serviceWorker.register('/P7-Restaurant-Reviews-App/sw.js')
   .then(function(registration) {
     console.log('ServiceWorker registration successful, scope is: ', registration.scope);
   })
